1a77f7b09b4aec7942ef81639a4c8f42dde97836
Douglas Gomes <douglasgommez@gmail.com>
2021-10-20 21:44:25 -0300

    Created a composable function that put a TextField and a button on screen, and the text input it's shown in a snackbar

--- app/src/main/java/com/odougle/composefirtsteps/MainActivity.kt
+++ app/src/main/java/com/odougle/composefirtsteps/MainActivity.kt
@@ -9,14 +9,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
@@ -27,18 +25,69 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.odougle.composefirtsteps.ui.theme.ComposeFirtStepsTheme
+import kotlinx.coroutines.launch
 import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ColorBox(Modifier.fillMaxSize())
+            TextFieldAndSnackBar()
         }
     }
 }
 
-@SuppressLint("UnrememberedMutableState")
+
+@Composable
+fun TextFieldAndSnackBar(){
+    val scaffoldState = rememberScaffoldState()
+    var textFieldState by remember{
+        mutableStateOf("")
+    }
+
+    var scope = rememberCoroutineScope()
+
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize(),
+        scaffoldState = scaffoldState
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(
+                    horizontal = 30.dp
+                )
+        ) {
+            TextField(
+                value = textFieldState,
+                label = {
+                    Text("Enter your name")
+                },
+                onValueChange = {
+                    textFieldState = it
+                },
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = {
+                scope.launch {
+                    scaffoldState.snackbarHostState.showSnackbar(
+                        "Hello $textFieldState"
+                    )
+
+                }
+            }) {
+                Text(text = "Please greet me")
+            }
+        }
+    }
+}
+
+
 @Composable
 fun ColorBox(
     modifier: Modifier = Modifier
