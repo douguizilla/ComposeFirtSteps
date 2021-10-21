639e4e2994c2432a9465e274d912853e8118ce82
Douglas Gomes <douglasgommez@gmail.com>
2021-10-20 21:14:22 -0300

    Created a composable function that uses state concept

--- app/src/main/java/com/odougle/composefirtsteps/MainActivity.kt
+++ app/src/main/java/com/odougle/composefirtsteps/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.odougle.composefirtsteps
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusModifier
@@ -23,14 +27,38 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.odougle.composefirtsteps.ui.theme.ComposeFirtStepsTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            ColorBox(Modifier.fillMaxSize())
         }
     }
+}
+
+@SuppressLint("UnrememberedMutableState")
+@Composable
+fun ColorBox(
+    modifier: Modifier = Modifier
+){
+    val color = remember {
+        mutableStateOf(Color.Yellow)
+    }
+
+    Box(
+        modifier = modifier
+            .background(color.value)
+            .clickable {
+                color.value = Color(
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    1f
+                )
+            }
+    )
 }
 
 
